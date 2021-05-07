23c3c6b20a4f05799d4b0b567f970c0e1b03190e
William Casazza <willcasazza@gmail.com>
2021-05-07 09:45:40 -0700

    common factor GWAS sstart

---
+++ delahaye_mQTL_analyses/r_scripts/common_factor_GWAS.R
@@ -0,0 +1,7 @@
+library(GenomicSEM)
+library(data.table)
+LDSCoutput <- readRDS("/scratch/st-dennisjk-1/wcasazza/sex_specific_mQTL/data/LDSC_covariances.RDS")
+p_sumstats <- fread("/scratch/st-dennisjk-1/wcasazza/sex_specific_mQTL/delahaye_mQTL_analyses/common_factor_sumstats_14_traits.txt.gz")
+pfactor <- commonfactorGWAS(covstruc = LDSCoutput, SNPs = p_sumstats)
+
+fwrite(pfactor,"/scratch/st-dennisjk-1/wcasazza/sex_specific_mQTL/data/common_factor_gwas_result.txt.gz",quote=F,row.names=F,sep='\t')
