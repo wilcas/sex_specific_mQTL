0a16b64bbe43b949f68d8f8c704e8e863b89db68
William Casazza <willcasazza@gmail.com>
2021-05-28 10:01:33 -0700

    added path diagram, updated CAVIAR script as stdout printout overwhelmed server

--- delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
+++ delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
@@ -6,8 +6,8 @@ for cur_annot in ${ANNOT[@]}; do
   z_score_job=$(qsub -v MQTLS="${cur_mqtl}",Z_DIR="${cur_annot}" run_caviar_per_chromosome.pbs)
 
   
-  mqtl_job=$(qsub -W depend=afterok:${z_score_job} -v MQTL="${cur_mqtl}",Z_DIR="/scratch/st-dennisjk-1/wcasazza/delahaye_QC/caviar_files/${cur_annot}",FILE_PREFIX="/scratch/st-dennisjk-1/wcasazza/1000G_phase3_ldsc/single_delahaye_annotations/${cur_annot}" run_caviar_merge.pbs) 
+  #mqtl_job=$(qsub -W depend=afterok:${z_score_job} -v MQTL="${cur_mqtl}",Z_DIR="/scratch/st-dennisjk-1/wcasazza/delahaye_QC/caviar_files/${cur_annot}",FILE_PREFIX="/scratch/st-dennisjk-1/wcasazza/1000G_phase3_ldsc/single_delahaye_annotations/${cur_annot}" run_caviar_merge.pbs) 
 
-  qsub -W depend=afterok:${mqtl_job} -v ANNOTATIONS="${cur_annot}" get_ldsc.pbs
+  #qsub -W depend=afterok:${mqtl_job} -v ANNOTATIONS="${cur_annot}" get_ldsc.pbs
 
 done
