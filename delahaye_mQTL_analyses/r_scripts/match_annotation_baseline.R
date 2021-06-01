47d3350b94d17d7abd1e8170ae1ba0b24960adb0
William Casazza <willcasazza@gmail.com>
2021-06-01 12:30:28 -0700

    updated caviar pipeline to work with 1000G EUR, fixed too many files problem

--- delahaye_mQTL_analyses/r_scripts/match_annotation_baseline.R
+++ delahaye_mQTL_analyses/r_scripts/match_annotation_baseline.R
@@ -8,7 +8,8 @@ annot_str <- argv[[1]]
 annot <- fread(sprintf(annot_str, i))
 annot$CM <- NULL
 annot <- annot[!duplicated(SNP)]
-bim <- fread(sprintf("/arc/project/st-dennisjk-1/shared/data/1000G_phase3_by_chr/plink/mapped_maf01_chr%d.bim", i))[, .(V1, V4, V2, V3)]
+bim <- fread(sprintf("/arc/project/st-dennisjk-1/shared/data/1000G_EUR_ldsc_data/1000G_EUR_Phase3_plink/1000G.EUR.QC.%d.bim", i))[, .(V1, V4, V2, V3)]
+#bim <- fread(sprintf("/arc/project/st-dennisjk-1/shared/data/1000G_phase3_by_chr/plink/mapped_maf01_chr%d.bim", i))[, .(V1, V4, V2, V3)]
 colnames(bim) <- c("CHR", "BP", "SNP", "CM")
 merged <- merge(bim, annot, by = colnames(bim)[-c(4)], all.x = TRUE)
 setnafill(merged, fill = 0, cols = 5:ncol(merged))
--- delahaye_mQTL_analyses/r_scripts/prep_caviar_z.R
+++ delahaye_mQTL_analyses/r_scripts/prep_caviar_z.R
@@ -1,7 +1,6 @@
 library(data.table)
-library(parallel)
+
 argv <- commandArgs(trailingOnly = TRUE)
-probe_pos <- fread("matrix_eqtl_data/probe_pos.txt")
 kg_snps <- fread(
   "/arc/project/st-dennisjk-1/shared/data/1000G_phase3_info/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz",
   skip = "#CHROM",
@@ -9,14 +8,14 @@ kg_snps <- fread(
 )
 all_mQTLs <- fread(argv[[2]])
 mQTL_in_1kg <- all_mQTLs[SNP %chin% kg_snps[`#CHROM` == argv[[1]]]$ID]
-out <- mclapply(
+out <- lapply(
   unique(mQTL_in_1kg$gene),
   function(cpg) {
     dt <- mQTL_in_1kg[gene == cpg & `p-value` < 0.05, .(SNP, Z = `t-stat`)]
     if (nrow(dt) > 0) {
       fwrite(
         dt,
-        sprintf("caviar_files/%s/chr%s/%s.z", argv[[3]], argv[[1]], cpg),
+        sprintf("%s/chr%s/%s.z", argv[[3]], argv[[1]], cpg),
         sep = "\t",
         col.names = FALSE,
         quote = FALSE
@@ -25,6 +24,5 @@ out <- mclapply(
     } else {
       return(data.table())
     }
-  },
-  mc.cores = 16
+  }
 )
