922bf5584823415751183b7ed51b01a9950a3e2b
William Casazza <willcasazza@gmail.com>
2021-06-09 13:10:53 -0700

    updated with EUR analysis

--- delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
+++ delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-ANNOT=( "male_meta" "female_meta" "marginal_meta" "sex_interaction_meta" )
+ANNOT=( "cord_mqtl" ) # "male_meta" "female_meta" "marginal_meta" "sex_interaction_meta" )
 
 for cur_annot in ${ANNOT[@]}; do 
-  cur_mqtl="/scratch/st-dennisjk-1/wcasazza/sex_specific_mQTL/data/delahaye_richs_${cur_annot}_matrixeqtl.txt" 
+  cur_mqtl="/scratch/st-dennisjk-1/wcasazza/ariesmqtl/cord.ALL.M.tab" 
   mqtl_job=$(qsub -v MQTLS="${cur_mqtl}",Z_DIR="/tmp/${cur_annot}",FILE_PREFIX="/scratch/st-dennisjk-1/wcasazza/1000G_phase3_ldsc/single_delahaye_annotations/${cur_annot}" run_caviar_per_chromosome.pbs)
 
   qsub -W depend=afterok:${mqtl_job} -v ANNOTATIONS="${cur_annot}" get_ldsc.pbs
+  #qsub  -v ANNOTATIONS="${cur_annot}" get_ldsc.pbs
 
 done
--- delahaye_mQTL_analyses/r_scripts/merge_baseline.R
+++ delahaye_mQTL_analyses/r_scripts/merge_baseline.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(parallel)
 
-setwd("/scratch/st-dennisjk-1/wcasazza/1000G_phase3_ldsc/baseline_annot_files/")
+setwd("/scratch/st-dennisjk-1/wcasazza/1000G_phase3_ldsc/baseline_EUR_annot_files/")
 
 out <- mclapply(
   1:22,
@@ -11,7 +11,7 @@ out <- mclapply(
     dt <- do.call(cbind, lapply(files, function(f) fread(f, header = T)))
     colnames(dt) <- names
     dt$base <- 1
-    bim <- fread(sprintf("/project/st-dennisjk-1/shared/data/1000G_phase3_by_chr/plink/mapped_maf01_chr%d.bim", i), header = F)
+    bim <- fread(sprintf("/project/st-dennisjk-1/shared/data/1000G_EUR_ldsc_data/1000G_EUR_Phase3_plink/1000G.EUR.QC.%d.bim", i), header = F)
     bim <- bim[, .(CHR = i, BP = V4, SNP = V2, CM = V3)]
     fwrite(cbind(bim, dt), sprintf("baselineLD.%d.annot.gz", i), sep = "\t", col.names = T, row.names = F, quote = F)
   },
