f9fc47e5666afdaa9c1a3f6b2b32d1008b6ddfa7
William Casazza <willcasazza@gmail.com>
2021-06-23 17:37:08 -0700

    analysis for stratified ldsc validation and exploration, heritability for r new annotation

--- delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
+++ delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ANNOT=( "cord_mqtl" ) # "male_meta" "female_meta" "marginal_meta" "sex_interaction_meta" )
+ANNOT=( "male_specific_meta" "female_specific_meta" ) # "cord_mqtl" "male_meta" "female_meta" "marginal_meta" "sex_interaction_meta" )
 
 for cur_annot in ${ANNOT[@]}; do 
   cur_mqtl="/scratch/st-dennisjk-1/wcasazza/ariesmqtl/cord.ALL.M.tab" 
-  mqtl_job=$(qsub -v MQTLS="${cur_mqtl}",Z_DIR="/tmp/${cur_annot}",FILE_PREFIX="/scratch/st-dennisjk-1/wcasazza/1000G_phase3_ldsc/single_delahaye_annotations/${cur_annot}" run_caviar_per_chromosome.pbs)
+  #mqtl_job=$(qsub -v MQTLS="${cur_mqtl}",Z_DIR="/tmp/${cur_annot}",FILE_PREFIX="/scratch/st-dennisjk-1/wcasazza/1000G_phase3_ldsc/single_delahaye_annotations/${cur_annot}" run_caviar_per_chromosome.pbs)
 
   #qsub -W depend=afterok:${mqtl_job} -v ANNOTATIONS="${cur_annot}" get_ldsc.pbs
   qsub  -v ANNOTATIONS="${cur_annot}" get_ldsc.pbs
