0bf2ce4b01d49fb1157bed7c27aa4905d1375c2c
William Casazza <willcasazza@gmail.com>
2021-07-26 15:30:49 -0700

    adding cleanup line

--- delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
+++ delahaye_mQTL_analyses/pbs_scripts/run_caviar_pipeline.sh
@@ -8,7 +8,7 @@ for cur_annot in ${ANNOT[@]}; do
 	else
 		cur_mqtl="/scratch/st-dennisjk-1/wcasazza/sex_specific_mQTL/data/delahaye_richs_${cur_annot}_matrixeqtl.txt"
 	fi
-	mqtl_job=$(qsub -v MQTLS="${cur_mqtl}",Z_DIR="/tmp/${cur_annot}",FILE_PREFIX="/scratch/st-dennisjk-1/wcasazza/sex_specific_mQTL/data/${cur_annot}" run_caviar_detection_per_chromosome.pbs)
+	mqtl_job=$(qsub -v ANNOT_DIR="${cur_annot}",MQTLS="${cur_mqtl}",Z_DIR="/tmp/${cur_annot}",FILE_PREFIX="/scratch/st-dennisjk-1/wcasazza/sex_specific_mQTL/data/${cur_annot}" run_caviar_detection_per_chromosome.pbs)
 
 	#qsub -W depend=afterok:${mqtl_job} -v ANNOTAsTIONS="${cur_annot}" get_ldsc.pbs
 	#qsub -v ANNOTATIONS="${cur_annot}" get_ldsc.pbs
